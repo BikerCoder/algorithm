ba80ed8f3e8bac57ec0bb178526738e3fa0c9cec
bikercoder <1312556151@qq.com>
2021-01-06 21:18:01 +0800

    求最大公约数-辗转相除发和更相减损术结合的方法

--- src/main/java/com/bikercoder/study/algorithm/excise/GreatestCommonDivisor.java
+++ src/main/java/com/bikercoder/study/algorithm/excise/GreatestCommonDivisor.java
@@ -39,7 +39,32 @@ public class GreatestCommonDivisor {
 
     }
 
+    /**
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int gcd(int a, int b){
+        if(a == b){
+            return a;
+        }
+        if((a&1)==0 && (b&1)==0){
+            return gcd(a>>1,b>>1)<<1;
+        } else if((a&1)==0 && (b&1)!=0) {
+            return gcd(a>>1,b);
+        } else if((a&1)!=0 && (b&1)==0) {
+            return gcd(a,b>>1);
+        } else{
+            int big = a > b ? a : b;
+            int small = a > b ? b : a;
+            return gcd(big-small,small);
+        }
+
+
+    }
+
     public static void main(String[] args) {
-        System.out.println(getGreatestCommonDivisorV3(100,80));
+        System.out.println(gcd(100,80));
     }
 }

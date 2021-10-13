6a452aafe8317d2089b32ae06e142bf0a3475c4c
caoguo <1312556151@qq.com>
2021-10-13 17:23:02 +0800

    88.合并两个有序数组

---
+++ src/main/java/com/bikercoder/study/algorithm/array/MergeSortedArray.java
@@ -0,0 +1,48 @@
+package com.bikercoder.study.algorithm.array;
+
+import java.util.Arrays;
+
+/**
+ * @author caoguo
+ * @Description： 力扣 88.合并两个有序数组 https://leetcode-cn.com/problems/merge-sorted-array/
+ * @date：2021/10/13 5:08 下午
+ **/
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1, 2, 3, 0, 0, 0};
+        int[] nums2 = new int[]{2, 5, 6};
+        merge(nums1, 3, nums2, 3);
+    }
+
+    /**
+     * 双指针 时间复杂度O(m+n) 空间复杂度O(m+n)
+     *
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = 0;
+        int p2 = 0;
+        int cur;
+        int[] sort = new int[m + n];
+        while (p1 < m || p2 < n) {
+            if (p1 == m) {
+                cur = nums2[p2++];
+            } else if (p2 == n) {
+                cur = nums1[p1++];
+            } else if (nums1[p1] <= nums2[p2]) {
+                cur = nums1[p1++];
+            } else {
+                cur = nums2[p2++];
+            }
+            sort[p1 + p2 - 1] = cur;
+        }
+        for (int i = 0; i < m + n; i++) {
+            nums1[i] = sort[i];
+        }
+        System.out.println(Arrays.toString(nums1));
+    }
+
+}

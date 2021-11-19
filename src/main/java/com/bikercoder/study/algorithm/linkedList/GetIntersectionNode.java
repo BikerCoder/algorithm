08357757b8dacfe6a188f86e7c38edbe10334e82
caoguo <1312556151@qq.com>
2021-11-19 23:07:22 +0800

    160. 相交链表 - 双指针

--- src/main/java/com/bikercoder/study/algorithm/linkedList/GetIntersectionNode.java
+++ src/main/java/com/bikercoder/study/algorithm/linkedList/GetIntersectionNode.java
@@ -16,8 +16,8 @@ public class GetIntersectionNode {
         headA.next.next.next = new ListNode(5);
         ListNode headB = new ListNode(1);
         headB.next = headA.next;
-        headB.next.next = headA.next.next;
-        System.out.println(getIntersectionNode(headA, headB).val);
+        headB.next.next = new ListNode(8);
+        System.out.println(getIntersectionNode2(headA, headB).val);
     }
 
     /**
@@ -44,5 +44,24 @@ public class GetIntersectionNode {
             temp = temp.next;
         }
         return null;
+    }
+
+    /**
+     * 双指针 走到尽头见不到你，于是走过你来时的路，等到相遇时才发现，你也走过我来时的路。
+     * 时间复杂度O(n+m)
+     * 空间复杂度O(1)
+     *
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public static ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
     }
 }

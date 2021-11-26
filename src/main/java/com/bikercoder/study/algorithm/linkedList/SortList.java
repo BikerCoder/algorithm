314e52fa01a7e3e8875997e10c8e764402b2b23b
caoguo <1312556151@qq.com>
2021-11-26 22:17:38 +0800

    148. 排序链表 - 自底向上归并排序

--- src/main/java/com/bikercoder/study/algorithm/linkedList/SortList.java
+++ src/main/java/com/bikercoder/study/algorithm/linkedList/SortList.java
@@ -12,7 +12,7 @@ public class SortList {
         node.next = new ListNode(3);
         node.next.next = new ListNode(5);
         node.next.next.next = new ListNode(2);
-        sortList(node);
+        sortList2(node);
         while (node != null) {
             System.out.println(node.val);
             node = node.next;
@@ -49,5 +49,47 @@ public class SortList {
         ListNode left = sortList(head, mid);
         ListNode right = sortList(mid, tail);
         return MergeTwoLists.mergeTwoLists2(left, right);
+    }
+
+    /**
+     * 自底向上归并排序 时间复杂度O(nlogn) 空间复杂度O(1)
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode sortList2(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode dummyHead = new ListNode(0, head);
+        ListNode temp = head;
+        int len = 0;
+        while (temp != null) {
+            len++;
+            temp = temp.next;
+        }
+        for (int i = 1; i < len; i <<= 1) {
+            ListNode prev = dummyHead;
+            ListNode curr = dummyHead.next;
+            while (curr != null) {
+                ListNode a = curr;
+                ListNode b = splitList(a, i);
+                curr = splitList(b, i);
+                prev.next = MergeTwoLists.mergeTwoLists2(a, b);
+                while (prev.next != null) {
+                    prev = prev.next;
+                }
+            }
+
+        }
+        return dummyHead.next;
+    }
+
+    private static ListNode splitList(ListNode head, int step) {
+        if (head == null) return head;
+        for (int i = 1; i < step && head.next != null; i++) {
+            head = head.next;
+        }
+        ListNode res = head.next;
+        head.next = null;
+        return res;
     }
 }

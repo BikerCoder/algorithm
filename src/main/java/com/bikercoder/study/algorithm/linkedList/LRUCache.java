434f1486184be7545f044fb3814d3d2e81254a5a
caoguo <1312556151@qq.com>
2021-11-29 20:39:42 +0800

    146. LRU 缓存机制 添加注释

--- src/main/java/com/bikercoder/study/algorithm/linkedList/LRUCache.java
+++ src/main/java/com/bikercoder/study/algorithm/linkedList/LRUCache.java
@@ -23,30 +23,58 @@ public class LRUCache {
         tail.prev = head;
     }
 
+    /**
+     * 根据key获取value
+     *
+     * @param key
+     * @return
+     */
     public int get(int key) {
+        // 根据kye从缓存中获取节点，节点为空则返回-1
         DLinkedNode node = cache.get(key);
         if (node == null) return -1;
+        // 不为空则将节点移动到头部
         moveToHead(node);
+        // 返回value
         return node.value;
     }
 
+    /**
+     * 将kye-value存入cache
+     *
+     * @param key
+     * @param value
+     */
     public void put(int key, int value) {
+        // 先根据key去cache中获取节点
         DLinkedNode node = cache.get(key);
         if (node == null) {
+            // 节点为空，则新建节点，将key-node存入cache
             DLinkedNode newNode = new DLinkedNode(key, value);
             cache.put(key, newNode);
+            // 将节点添加到头部
+            addToHead(newNode);
+            // size++
             size++;
+            // 判断是否超出容量，超出容量则需要移除末尾节点
             if (size > capacity) {
                 DLinkedNode tail = removeTail();
                 cache.remove(tail.key);
                 size--;
             }
         } else {
+            // 如果节点不为空，则表示存在旧值，覆盖value
             node.value = value;
+            // 将节点移动到头部
             moveToHead(node);
         }
     }
 
+    /**
+     * 移除末尾节点
+     *
+     * @return
+     */
     private DLinkedNode removeTail() {
         DLinkedNode res = tail.prev;
         removeNode(res);
@@ -54,18 +82,33 @@ public class LRUCache {
         return res;
     }
 
+    /**
+     * 将节点移动到头部
+     *
+     * @param node
+     */
     private void moveToHead(DLinkedNode node) {
         removeNode(node);
-        addHead(node);
+        addToHead(node);
     }
 
-    private void addHead(DLinkedNode node) {
+    /**
+     * 添加节点到头部
+     *
+     * @param node
+     */
+    private void addToHead(DLinkedNode node) {
         node.next = head.next;
         head.next.prev = node;
         node.prev = head;
         head.next = node;
     }
 
+    /**
+     * 移除节点
+     *
+     * @param node
+     */
     private void removeNode(DLinkedNode node) {
         node.prev.next = node.next;
         node.next.prev = node.prev;

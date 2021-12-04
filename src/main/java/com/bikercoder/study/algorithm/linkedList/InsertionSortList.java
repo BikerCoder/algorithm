1e6d45ac2e0056480f7d0c4b651c9528b1492f87
caoguo <1312556151@qq.com>
2021-12-04 15:39:58 +0800

    147. 对链表进行插入排序 添加注释

--- src/main/java/com/bikercoder/study/algorithm/linkedList/InsertionSortList.java
+++ src/main/java/com/bikercoder/study/algorithm/linkedList/InsertionSortList.java
@@ -28,17 +28,22 @@ public class InsertionSortList {
         if (head == null) {
             return null;
         }
+        // 定义哑节点，方便再head前插入节点
         ListNode dummyHead = new ListNode(0);
         dummyHead.next = head;
+        // 定义有序链表的最后一个节点，初始为head；定义curr为需要插入有序链表的节点，初始为head.next
         ListNode lastSorted = head, curr = head.next;
         while (curr != null) {
+            // 如果lastSorted的值 <= curr的值，则表示两个值本身就是升序的，直接将lastSorted后移一位
             if (lastSorted.val <= curr.val) {
                 lastSorted = lastSorted.next;
             } else {
+                // 否则从dummyHead开始遍历，找到curr插入的前一个元素prev
                 ListNode prev = dummyHead;
                 while (prev.next.val <= curr.val) {
                     prev = prev.next;
                 }
+                // curr插入到有序链表中
                 lastSorted.next = curr.next;
                 curr.next = prev.next;
                 prev.next = curr;

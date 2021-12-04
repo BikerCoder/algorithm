5eb03cff6c9cbf2f1301af3cf112ed879c92e67c
caoguo <1312556151@qq.com>
2021-12-04 13:03:56 +0800

    287. 寻找重复数 - 二分法

---
+++ src/main/java/com/bikercoder/study/algorithm/binarySearch/FindDuplicate.java
@@ -0,0 +1,40 @@
+package com.bikercoder.study.algorithm.binarySearch;
+
+/**
+ * @author caoguo
+ * @Description： 287. 寻找重复数 https://leetcode-cn.com/problems/find-the-duplicate-number/
+ * @date：2021/12/4 11:47 上午
+ **/
+public class FindDuplicate {
+    public static void main(String[] args) {
+        System.out.println(findDuplicate(new int[]{1, 2, 3, 3, 4}));
+    }
+
+    /**
+     * 二分法
+     * 时间复杂度
+     * 空间复杂度O(1)
+     * @param nums
+     * @return
+     */
+    public static int findDuplicate(int[] nums) {
+        int n = nums.length;
+        int left = 0, right = n - 1, ans = -1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            int cnt = 0;
+            for (int i = 0; i < n; i++) {
+                if (nums[i] <= mid) {
+                    cnt++;
+                }
+            }
+            if (cnt <= mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+                ans = mid;
+            }
+        }
+        return ans;
+    }
+}
